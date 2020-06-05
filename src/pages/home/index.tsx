2e62c01beb68162df8add40cb114468304d0cdce
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-06-04 23:46:48 -0300

    Update - Registration page with all fields

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiLogIn} from 'react-icons/fi'
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -16,10 +17,10 @@ const Home = () => {
                 <main>
                     <h1>Seu marketplace de coleta de resídious</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-                    <a href="/cadastro">
+                    <Link to="/create-point">
                         <span><FiLogIn /></span>
                         <strong>Cadastre um ponto de coleta</strong>
-                    </a>
+                    </Link>
                 </main>
             </div>
         </div>
